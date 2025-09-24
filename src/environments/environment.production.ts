a67dce641686f8fcbcada120d779e6424bc35cae
Putri Nur Rahmah <putrienrah@gmail.com>
2025-09-24 23:36:15 +0700

    Update env

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -4,4 +4,8 @@ export const environment = {
   API_URL: 'http://localhost:3000/api/v1',
   locale_string: 'IDR',
   default_currency_code: 'id',
+  SECRET_KEY:
+    '3f54557a3fd4c2398564138e1de844c0b7ad1918694f542ce13f2638319a38d7',
+  midtransSnapApiUrl: 'https://app.sandbox.midtrans.com/snap/snap.js',
+  midtransClientKey: 'SB-Mid-client-jaDojgLpZuIoiHs_',
 };
