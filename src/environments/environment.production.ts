a4482be05e2246209e2cd1c7aa3e0ac3da0fad56
Putri Nur Rahmah <putrienrah@gmail.com>
2025-09-29 15:58:16 +0700

    Update env

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   appName: 'PROMANSYS WEB',
-  // API_URL: 'http://localhost:3000/api/v1',
-  API_URL: 'https://proman.folxcode.dev/api/v1',
+  API_URL: 'http://localhost:3000/api/v1',
+  // API_URL: 'https://proman.folxcode.dev/api/v1',
   locale_string: 'IDR',
   default_currency_code: 'id',
   SECRET_KEY:
